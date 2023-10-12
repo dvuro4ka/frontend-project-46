645fb2c0a546aad34547a64edbd5bfb18bc9410c
dvuro4ka <gven.stacy@gmail.com>
2023-10-12 22:11:21 +0300

    step 8, add tests

--- __test__/test.js
+++ __test__/test.js
@@ -1,3 +1,29 @@
-test('gendiff', () => {
-  expect(100).toEqual(100);
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedStylish = readFile('expectedStylish.txt');
+const expectedPlain = readFile('expectedPlain.txt');
+
+const cases = [
+  ['file1.json', 'file2.json', 'stylish', expectedStylish],
+  ['file3.yml', 'file4.yml', 'stylish', expectedStylish],
+  ['file5.yaml', 'file6.yaml', 'stylish', expectedStylish],
+  ['file1.json', 'file2.json', 'plain', expectedPlain],
+  ['file3.yml', 'file4.yml', 'plain', expectedPlain],
+  ['file5.yaml', 'file6.yaml', 'plain', expectedPlain],
+];
+
+describe('All tests', () => {
+  test.each(cases)('Format %s', (file1, file2, format, expectedFile) => {
+    const actual = gendiff(getFixturePath(file1), getFixturePath(file2), format);
+    expect(actual).toEqual(expectedFile);
+  });
 });
